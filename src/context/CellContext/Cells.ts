3e20d002c6f27103adeb48e5981c568ad154074d
Marlonn Locatelli <marlonn@gmail.com>
2026-09-07 18:21:16 -0300

    feat: added cells

---
+++ src/context/CellContext/Cells.ts
@@ -0,0 +1,106 @@
+import type { CellModel } from '../../models/CellModel'
+
+export const CellsDB: CellModel[] = [
+  {
+    name: {
+      full: 'Neutrófilo segmentado',
+      name: 'Segmentado',
+      abr: 'seg',
+    },
+    info: {
+      description: '',
+      image: '',
+    },
+    count: 0,
+    key: 'd',
+    color: '#e88fa6',
+    VR: '50-60',
+    group: 'WBC',
+    maturiry: 'mature',
+  },
+  {
+    name: {
+      full: 'Neutrófilo bastonete',
+      name: 'Bastão',
+      abr: 'bast',
+    },
+    info: {
+      description: '',
+      image: '',
+    },
+    count: 0,
+    key: 's',
+    color: '#F5C9D3',
+    VR: '2-5',
+    group: 'WBC',
+    maturiry: 'immature',
+  },
+  {
+    name: {
+      full: 'Linfócito',
+      name: 'Linfócito',
+      abr: 'lin',
+    },
+    info: {
+      description: '',
+      image: '',
+    },
+    count: 0,
+    key: 'f',
+    color: '#5B9BD5',
+    VR: '40-60',
+    group: 'WBC',
+    maturiry: 'mature',
+  },
+  {
+    name: {
+      full: 'Monócito',
+      name: 'Monócito',
+      abr: 'mono',
+    },
+    info: {
+      description: '',
+      image: '',
+    },
+    count: 0,
+    key: 'g',
+    color: '#7B6FD1',
+    VR: '6-10',
+    group: 'WBC',
+    maturiry: 'mature',
+  },
+  {
+    name: {
+      full: 'Eosinófilo',
+      name: 'Eosinófilo',
+      abr: 'eos',
+    },
+    info: {
+      description: '',
+      image: '',
+    },
+    count: 0,
+    key: 'a',
+    color: '#E2725C',
+    VR: '1-10',
+    group: 'WBC',
+    maturiry: 'mature',
+  },
+  {
+    name: {
+      full: 'Basófilo',
+      name: 'Basófilo',
+      abr: 'baso',
+    },
+    info: {
+      description: '',
+      image: '',
+    },
+    count: 0,
+    key: 'q',
+    color: '#8FC9A9',
+    VR: '0-2',
+    group: 'WBC',
+    maturiry: 'mature',
+  },
+]
